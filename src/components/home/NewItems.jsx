38e11c3aa7c73d96e18bb5c0c004288d005bf4b1
Gamemaster <101598503+frostjelly@users.noreply.github.com>
2023-11-02 21:20:27 -0700

    Continued Internship. Worked on 'New Items' section. Added carousel feature. Fully Response.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -4,6 +4,10 @@ import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
 const NewItems = () => {
   const [data, setData] = useState(null);
 
@@ -30,8 +34,82 @@ const NewItems = () => {
             </div>
           </div>
 
-          {data &&
-            data.map((item, index) => (
+          {data && (
+            <OwlCarousel
+              className="owl-theme"
+              dotsEach
+              loop
+              nav
+              margin={10}
+              items={4}
+              responsive={{
+                1200: { items: 4 },
+                980: { items: 3 },
+                535: { items: 2 },
+                0: { items: 1 },
+              }}
+            >
+              {data.map((item, index) => (
+                <div className="" key={index}>
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link
+                        to="/author"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <img className="lazy" src={item.authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="de_countdown">5h 30m 32s</div>
+
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Link to="/item-details">
+                        <img
+                          src={item.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to="/item-details">
+                        <h4>{item.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{item.price} ETH</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{item.likes}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </OwlCarousel>
+          )}
+
+          {!data &&
+            new Array(4).fill(0).map((_, index) => (
               <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
                 <div className="nft__item">
                   <div className="author_list_pp">
@@ -41,7 +119,7 @@ const NewItems = () => {
                       data-bs-placement="top"
                       title="Creator: Monica Lucas"
                     >
-                      <img className="lazy" src={item.authorImage} alt="" />
+                      <img className="lazy" src={AuthorImage} alt="" />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
@@ -68,7 +146,7 @@ const NewItems = () => {
 
                     <Link to="/item-details">
                       <img
-                        src={item.nftImage}
+                        src={nftImage}
                         className="lazy nft__item_preview"
                         alt=""
                       />
@@ -76,74 +154,18 @@ const NewItems = () => {
                   </div>
                   <div className="nft__item_info">
                     <Link to="/item-details">
-                      <h4>{item.title}</h4>
+                      <h4>Pinky Ocean</h4>
                     </Link>
-                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_price">3.08 ETH</div>
                     <div className="nft__item_like">
                       <i className="fa fa-heart"></i>
-                      <span>{item.likes}</span>
+                      <span>69</span>
                     </div>
                   </div>
                 </div>
               </div>
             ))}
-
-          {!data && (new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )))}
+            
         </div>
       </div>
     </section>
