808fdf94b4d0d02c8e28208b7a956a761005ff9b
Gamemaster <101598503+frostjelly@users.noreply.github.com>
2023-11-03 21:10:47 -0700

    Continued Internship. Worked on Skeleton loading state for 'New Items' section. Work in progress.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -108,64 +108,73 @@ const NewItems = () => {
             </OwlCarousel>
           )}
 
-          {!data &&
-            new Array(4).fill(0).map((_, index) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-                <div className="nft__item">
-                  <div className="author_list_pp">
-                    <Link
-                      to="/author"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="Creator: Monica Lucas"
-                    >
-                      <img className="lazy" src={AuthorImage} alt="" />
+          {data && (
+            <OwlCarousel
+              className="owl-theme"
+              dotsEach
+              loop
+              nav
+              margin={10}
+              items={4}
+              responsive={{
+                1200: { items: 4 },
+                980: { items: 3 },
+                535: { items: 2 },
+                0: { items: 1 },
+              }}
+            >
+              {new Array(7).fill(0).map((_, index) => (
+                <div className="" key={index}>
+                  <div className="nft__item">
+                    <div className="author_list_pp  skeleton-avatar">
+                      <Link to="/author ">
+                        <div className="skeleton-box lazy pp-coll skeleton-avatar"></div>
+                        <img className="lazy pp-coll" src="" alt="" />
+                      </Link>
                       <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="de_countdown">5h 30m 32s</div>
-
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
-                        </div>
-                      </div>
                     </div>
 
-                    <Link to="/item-details">
-                      <img
-                        src={nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to="/item-details">
-                      <h4>Pinky Ocean</h4>
-                    </Link>
-                    <div className="nft__item_price">3.08 ETH</div>
-                    <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>69</span>
+                    <div className="author_list_pp skeleton-box skeleton-text skeleton-avatar rounded-circle w-25 h-25">
+                      <Link
+                        to="/author"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <img
+                          className="lazy w-25 h-25  skeleton-avatar skeleton-box skeleton-text"
+                          src=""
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div
+                      className="skeleton-box skeleton-text"
+                      style={{ textAlign: "end" }}
+                    >
+                      5h 30m 32s
+                    </div>
+
+                    <div className="nft__item_wrap">
+                      <div className="skeleton-box w-100 h-75"></div>
+                    </div>
+                    <div className="nft__item_info">
+                      <h4 className="skeleton-box skeleton-text">
+                        Pinky Ocean
+                      </h4>
+                      <div className="nft__item_price skeleton-box skeleton-text">
+                        3.08 ETH
+                      </div>
+                      <div className="nft__item_like">
+                        <i className="skeleton-box skeleton-text">6911</i>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-            
+              ))}
+            </OwlCarousel>
+          )}
         </div>
       </div>
     </section>
