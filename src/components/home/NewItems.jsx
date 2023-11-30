64d18ae6b851d10eb68222c0fb01f98df71dd14e
Gamemaster <101598503+frostjelly@users.noreply.github.com>
2023-11-29 21:07:52 -0800

    Continued Internship by working on dynamic links for 'New Items' section. Need to work on NFT timer. Work in progress.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -54,7 +54,7 @@ const NewItems = () => {
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
-                        to="/author"
+                        to={`/author/${item.authorId}`}
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
                         title="Creator: Monica Lucas"
@@ -84,7 +84,7 @@ const NewItems = () => {
                         </div>
                       </div>
 
-                      <Link to="/item-details">
+                      <Link to={`/item-details/${item.nftId}`}>
                         <img
                           src={item.nftImage}
                           className="lazy nft__item_preview"
